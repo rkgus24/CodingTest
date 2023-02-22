dc9e03cd407f8e10d334eb134d497fa5a279ae8e
가현 <102145605+rkgus24@users.noreply.github.com>
2023-02-22 21:36:43 +0900

    Add files via upload

---
+++ 23.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	int n, i, pre, now, cnt, max;
+
+	scanf("%d", &n);
+	scanf("%d", &pre);
+
+	cnt=1;
+	max=1;
+
+	for(i=2; i<=n; i++){
+		scanf("%d", &now);
+
+		if(now>=pre){
+			cnt++;
+			if(cnt>max) max=cnt;
+		}
+
+		else cnt=1;
+		pre=now;
+	}
+	printf("%d\n", max);	
+
+	return 0;
+}
